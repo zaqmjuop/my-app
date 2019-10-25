cf44284883678cef88fa72ba088a517c56815bdf
zaqmjuop <1060007045@qq.com>
2019-10-25 08:52:14 +0800

    Updates

---
+++ precache-manifest.066da5d01f42a025411d62a46a672e06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f161bf64a4d8e456a406cd4321a1be4c",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "958eda25e7df72e90f7e",
+    "url": "/myapp/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2c8eb76330bf77267e37",
+    "url": "/myapp/static/js/2.8b165efd.chunk.js"
+  },
+  {
+    "revision": "958eda25e7df72e90f7e",
+    "url": "/myapp/static/js/main.fc8e9ac6.chunk.js"
+  },
+  {
+    "revision": "11af245f4794d8a306c7",
+    "url": "/myapp/static/js/runtime~main.39bd4eaf.js"
+  }
+]);
